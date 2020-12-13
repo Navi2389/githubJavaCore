bfa2977205a4a7f37afbca569dff51470385d8a4
KolupaevIvan <koluivan@yandex.ru>
2020-12-13 15:34:54 +0300

    Выполнено практическое задание 3 JavaCoreGeekbrains

---
+++ src/task3Generalizations/Apple.java
@@ -0,0 +1,8 @@
+package task3Generalizations;
+
+public class Apple extends Fruit {
+    @Override
+    float getWeight() {
+        return 1.0f;
+    }
+}
---
+++ src/task3Generalizations/Fruit.java
@@ -0,0 +1,5 @@
+package task3Generalizations;
+
+public abstract class Fruit {
+    abstract float getWeight();
+}
